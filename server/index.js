c6301eab04018da7824ea8b2afefb199ab3f4c2d
Ab9 <106424063+Ab369@users.noreply.github.com>
2024-09-16 19:58:47 +0530

    a

--- server/index.js
+++ server/index.js
@@ -19,5 +19,5 @@ mongoose.connect(process.env.MONGO_URL);
 
 
 app.listen(process.env.PORT||5000, () => {
-    console.log('Server is listening on port'+PORT);
+    console.log('Server is listening on port'+process.env.PORT);
   });
